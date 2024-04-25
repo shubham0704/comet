e49c6129be6d198f8a8ddcb46f071c6be611d312
shubham0704 <sb42451112@gmail.com>
2024-04-25 18:11:12 -0500

    minor changes

--- scripts/download_obj3d.py
+++ scripts/download_obj3d.py
@@ -1,8 +1,11 @@
+import os
 import gdown
 from zipfile import ZipFile
-zip_loc = "/mnt/data/shubham/OBJ3D.zip"
+parent_dir = "/mnt/data/shubham/"
+zip_file = "OBJ3D.zip"
+zip_loc = os.path.join(parent_dir, zip_file)
 gdown.download(id="1XSLW3qBtcxxvV-5oiRruVTlDlQ_Yatzm", output=zip_loc)
-
+# final file path -> parent_dir/OBJ3D
 with ZipFile(zip_loc, 'r') as f:
-    f.extractall(zip_loc[:-3])
+    f.extractall(parent_dir)
 
